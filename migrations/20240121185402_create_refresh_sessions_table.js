13e81a5cdd870b116f5d34533e20622db06baff0
Oleh Vietrov <104677837+LordAsmodey@users.noreply.github.com>
2024-01-21 23:21:32 +0200

    Add migrations (#4)
    * Added migrations

--- migrations/20240121185402_create_refresh_sessions_table.js
+++ migrations/20240121185402_create_refresh_sessions_table.js
@@ -7,7 +7,7 @@ export async function up(knex) {
       id SERIAL PRIMARY KEY,
       "userId" INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
       refresh_token VARCHAR(400) NOT NULL,
-      finger_print VARCHAR(400) NOT NULL
+      finger_print VARCHAR(1000) NOT NULL
     );
   `);
 }
