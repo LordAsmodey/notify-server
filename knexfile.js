83e12f241da84053f531d772126b86535675e22f
Oleg <oleg94038@gmail.com>
2024-01-22 11:20:29 +0100

    Add knex production-mode settings

--- knexfile.js
+++ knexfile.js
@@ -16,3 +16,19 @@ export const development = {
     directory: './migrations',
   },
 };
+
+export const production = {
+  client: 'pg',
+  connection: {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    ssl: process.env.DATABASE_SSL ? { rejectUnauthorized: false } : false,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: './migrations',
+  },
+};
