83b6979b226dbf5089cc076c43917800714201e2
Oleg <oleg94038@gmail.com>
2024-01-22 11:41:35 +0100

    Fix ssl in DB setings

--- src/db.js
+++ src/db.js
@@ -11,6 +11,7 @@ const connection = {
     database: process.env.DATABASE_NAME,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
+    ssl: process.env.DATABASE_SSL ? { rejectUnauthorized: false } : false,
 };
 
 export const db = pgp(connection);
